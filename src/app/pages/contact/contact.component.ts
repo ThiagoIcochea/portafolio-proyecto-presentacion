6af9930ae0aa47d8647d175c60b9075e80865873
ThiagoIcochea <U22330428@utp.edu.pe>
2026-07-24 12:32:23 -0500

    Agregando nuevos proyectos

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -14,7 +14,7 @@ export class ContactComponent {
 
  
   reasons = [
-    { icon: '🎓', title: 'Prácticas profesionales', desc: 'Estoy buscando oportunidades para iniciar mi carrera profesional.' },
+    { icon: '🎓', title: 'Prácticas preprofesionales', desc: 'Estoy buscando oportunidades para iniciar mi carrera preprofesional.' },
     { icon: '🚀', title: 'Proyectos Independientes',     desc: 'Disponible para proyectos part-time o full-time según disponibilidad.' },
     { icon: '🤝', title: 'Colaboración',            desc: 'Siempre abierto a colaborar con otros desarrolladores.' },
   ];
